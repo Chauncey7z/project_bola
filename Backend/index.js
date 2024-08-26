6c062a4de2ff8c33a2feec94cf862a192c81359e
Rico Yapieter <caopi3407@gmail.com>
2024-08-26 15:26:13 +0700

    fix hiding

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,3 @@
 const app = require('./app');
 const db = require('./config/connection');
 const userModel = require('./model/user_model');
-
-app.get('/', (req, res) => {
-    res;
-});
-
-const uri = db.uri;
-
-app.listen(3000, () => {
-    console.log('Server Listening on Port ${uri}');
-});
